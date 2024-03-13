5743309d4719bcfda009d6872e09a07fb6cf5e24
Mus <138971833+Musxeto@users.noreply.github.com>
2024-03-13 15:33:36 +0500

    lesgo everything setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,7 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="app">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
-  );
+  return <div className="app"></div>;
 }
 
 export default App;
