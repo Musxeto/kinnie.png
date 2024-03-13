ab1550b117a5b11d8aaee5c0f5c13688c191f4cb
Mus <138971833+Musxeto@users.noreply.github.com>
2024-03-13 15:12:39 +0500

    setting up tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="app">hello world</div>;
+  return (
+    <div className="app">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
 }
 
 export default App;
