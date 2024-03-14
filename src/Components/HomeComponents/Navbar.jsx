922db32db18d94ada40de44a9f48b329be0f76f0
Mus <138971833+Musxeto@users.noreply.github.com>
2024-03-14 11:02:54 +0500

    upload form

--- src/Components/HomeComponents/Navbar.jsx
+++ src/Components/HomeComponents/Navbar.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const Navbar = () => {
   return (
     <div className="navbar bg-base-100 justify-between">
-      <a className="heading font-bold text-4xl">m u s . p n g 📷 </a>
-      <button className="btn btn-primary">Logout</button>
+      <a className="heading font-bold text-3xl">m u s . p n g 📷 </a>
+      <button className="btn btn-ghost btn-outline">Logout</button>
     </div>
   );
 };
