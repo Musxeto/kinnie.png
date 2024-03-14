259f29f0095c8c8d4ba041d54e8f2621d098afd0
Mus <138971833+Musxeto@users.noreply.github.com>
2024-03-14 11:08:25 +0500

    home page ui completed

--- src/Components/HomeComponents/ImageGallery.jsx
+++ src/Components/HomeComponents/ImageGallery.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const ImageGallery = () => {
-  return <div></div>;
+  return (
+    <div className="grid md:grid-cols-3 justify-center gap-4 mt-10">
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <figure className="px-10 pt-10">
+          <img src="" alt="Shoes" className="rounded-xl" />
+        </figure>
+        <div className="card-body items-center text-center">
+          <h2 className="card-title">Image Title</h2>
+          <p>Uploaded By: </p>
+          <span>Created On:</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ImageGallery;
