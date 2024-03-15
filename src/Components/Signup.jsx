4f1a3a4ffbc57dd3dacf2d457c6286d77b0f779b
Mus <138971833+Musxeto@users.noreply.github.com>
2024-03-15 10:50:28 +0500

    sign up form

--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -1,7 +1,51 @@
 import React from "react";
-
+import { Form, Button, Card } from "react-bootstrap";
 const Signup = () => {
-  return <div></div>;
+  return (
+    <>
+      <Card>
+        <Card.Body>
+          <h2 className="text-center mb-4">Sign Up</h2>
+          <Form>
+            <Form.Group id="username">
+              <Form.Label>Username:</Form.Label>
+              <Form.Control
+                type="text"
+                ref={usernameRef}
+                required
+              ></Form.Control>
+            </Form.Group>
+            <Form.Group id="email">
+              <Form.Label>Email:</Form.Label>
+              <Form.Control type="email" ref={emailRef} required></Form.Control>
+            </Form.Group>
+            <Form.Group id="password">
+              <Form.Label>Password:</Form.Label>
+              <Form.Control
+                type="password-"
+                ref={passwordRef}
+                required
+              ></Form.Control>
+            </Form.Group>
+            <Form.Group id="password-confirm">
+              <Form.Label>Confirm Password:</Form.Label>
+              <Form.Control
+                type="password"
+                ref={passwordConfirmRef}
+                required
+              ></Form.Control>
+            </Form.Group>
+            <Button className="w-100" type="submit">
+              Sign Up
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+      <div className="w-100 text-center mt-2">
+        Already have an account? <a>Log In</a>
+      </div>
+    </>
+  );
 };
 
 export default Signup;
