8cff624dbef9c2d717eefb30413d22d9e143d925
Mus <138971833+Musxeto@users.noreply.github.com>
2024-03-16 11:10:55 +0500

    private routes completed

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,31 @@ import Home from "./Components/Home";
 import { AuthProvider } from "./Contexts/AuthContext";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./Components/Login";
+import PrivateRoute from "./Components/PrivateRoute";
+import UpdateProfile from "./Components/UpdateProfile";
 function App() {
   return (
     <>
       <AuthProvider>
         <BrowserRouter>
           <Routes>
-            <Route exact path="/" element={<Home />} />
+            <Route
+              exact
+              path="/"
+              element={
+                <PrivateRoute>
+                  <Home />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/update-profile"
+              element={
+                <PrivateRoute>
+                  <UpdateProfile />
+                </PrivateRoute>
+              }
+            />
             <Route path="/signup" element={<Signup />} />
             <Route path="/login" element={<Login />} />
           </Routes>
--- src/Components/PrivateRoute.jsx
+++ src/Components/PrivateRoute.jsx
@@ -3,14 +3,13 @@ import React from "react";
 import { Navigate, Route } from "react-router-dom";
 import { useAuth } from "../Contexts/AuthContext";
 
-const PrivateRoute = ({ element, ...rest }) => {
+const PrivateRoute = ({ children }) => {
   const { currentUser } = useAuth();
-
-  return currentUser ? (
-    <Route path="/" {...rest} element={element} />
-  ) : (
-    <Navigate to="/login" replace />
-  );
+  if (currentUser !== null) {
+    return children;
+  } else {
+    return <Navigate to={"/login"} />;
+  }
 };
 
 export default PrivateRoute;
